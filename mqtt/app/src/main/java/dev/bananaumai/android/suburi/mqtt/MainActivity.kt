088d4336827b6d54678f7ff01ca0c0702da2cce1
YUTA SHIMAKAWA <banana.umai@gmail.com>
2019-12-12 08:58:19 +0900

    simplify mqtt package

--- mqtt/app/src/main/java/dev/bananaumai/android/suburi/mqtt/MainActivity.kt
+++ mqtt/app/src/main/java/dev/bananaumai/android/suburi/mqtt/MainActivity.kt
@@ -1,69 +1,72 @@
 package dev.bananaumai.android.suburi.mqtt
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 import android.widget.TextView
-import androidx.lifecycle.Observer
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import org.koin.android.viewmodel.ext.android.viewModel
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private val state: StateViewModel by viewModel()
-    private val randomNumber: NumberViewModel by viewModel()
+    private val mqttServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            mqttService = (service as MqttService.MqttServiceBinder).getService()
+        }
 
-    private val randomNumberReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            if (intent != null) {
-                randomNumber.currentNumber.value = intent.getIntExtra("num", 0)
-            }
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mqttService = null
         }
     }
+
+    private var isRunning = false
+
+    private var mqttService: MqttService? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        LocalBroadcastManager.getInstance(this).registerReceiver(randomNumberReceiver, IntentFilter("randomNumber"))
         Intent(this, MqttService::class.java).also { intent ->
             startService(intent)
+            bindService(intent, mqttServiceConnection, Context.BIND_AUTO_CREATE)
         }
 
+        val text = findViewById<TextView>(R.id.textView)
         val button = findViewById<Button>(R.id.button)
-        state.currentState.observe(this, Observer<StateViewModel.State> { state ->
-            button.text = when (state) {
-                StateViewModel.State.RUNNING -> {
-                    Intent(this, EmitterService::class.java).also { intent ->
-                        startService(intent)
-                    }
-                    "RUNNING"
-                }
-                else -> {
-                    Intent(this, EmitterService::class.java).also { intent ->
-                        stopService(intent)
-                    }
-                    "STOPPED"
-                }
-            }
-        })
-        button.setOnClickListener { state.flip() }
+        button.setOnClickListener {
+            lifecycleScope.launch {
+                button.isEnabled = false
 
-        val randomNumberText = findViewById<TextView>(R.id.randomNumber)
-        randomNumber.currentNumber.observe(this, Observer<Int> { num ->
-            if (num == null) {
-                randomNumberText.text = ""
-            } else {
-                randomNumberText.text = num.toString()
+                if (isRunning) {
+                    mqttService?.disconnect()
+                } else {
+                    mqttService?.connect()
+                }
+
+                isRunning = !isRunning
+
+                if (isRunning) {
+                    text.text = "RUNNING"
+                    button.text = "STOP"
+                } else {
+                    text.text = "STOPPED"
+                    button.text = "START"
+                }
+
+                button.isEnabled = true
             }
-        })
+        }
     }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(randomNumberReceiver)
+        Intent(this, MqttService::class.java).also { intent ->
+            unbindService(mqttServiceConnection)
+            stopService(intent)
+        }
     }
 }
--- mqtt/app/src/main/java/dev/bananaumai/android/suburi/mqtt/MqttService.kt
+++ mqtt/app/src/main/java/dev/bananaumai/android/suburi/mqtt/MqttService.kt
@@ -1,122 +1,200 @@
 package dev.bananaumai.android.suburi.mqtt
 
 import android.app.Service
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence
 
 class MqttService : Service() {
-    private val serviceJob = Job()
-    private val serviceScope = CoroutineScope(Dispatchers.IO + serviceJob)
-    private val connectRetryCh = Channel<Boolean>()
-    private val connectRetryJob = serviceScope.launch {
-        Log.v("MqttService", "launch connect retry job")
-        while (connectRetryCh.receive()) {
-            Log.d("MqttService", "retry connect!")
-            connect()
+    inner class MqttServiceBinder : Binder() {
+        fun getService(): MqttService {
+            return this@MqttService
         }
     }
-    private val client: MqttAndroidClient by lazy {
-        val client = MqttAndroidClient(
-            this,
-            "tcp://10.0.2.2:1883", MqttClient.generateClientId()
-        )
 
-        client.setCallback(object : MqttCallbackExtended {
-            override fun connectComplete(reconnect: Boolean, serverURI: String?) {
-                Log.v("MqttService", "connectComplete")
-            }
+    private val binder = MqttServiceBinder()
 
-            override fun connectionLost(cause: Throwable?) {
-                Log.v("MqttService", "connectionLost")
-            }
+    private val job = SupervisorJob()
 
-            override fun deliveryComplete(token: IMqttDeliveryToken?) {
-                Log.v("MqttService", "deliveryComplete")
-            }
+    private val scope = CoroutineScope(job)
 
-            override fun messageArrived(topic: String?, message: MqttMessage?) {
-                Log.v("MqttService", "messageArrived")
-            }
-        })
+    private val mutex = Mutex()
 
-        client
-    }
-    private val randomNumberReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            Log.v("MqttService", "onReceive")
-            if (intent != null) {
-                val randomNum = intent.getIntExtra("num", 0)
-                if (client.isConnected) {
-                    Log.v("MqttService", "try to publish message with $randomNum")
-                    MqttMessage().also { msg ->
-                        msg.payload = randomNum.toString().toByteArray()
-                        client.publish("/test", msg)
-                    }
-                }
-            }
-        }
-    }
+    private lateinit var client: IMqttAsyncClient
 
     override fun onCreate() {
         super.onCreate()
-        Log.v("MqttServide", "onCreate")
+        client = createClient()
+    }
+
+    @ExperimentalCoroutinesApi
+    override fun onDestroy() {
+        super.onDestroy()
+
+        scope.launch {
+            disconnect()
+            job.cancel()
+        }
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        return null
+        return binder
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.v("MqttService", "onStartCommand")
-        connect()
+        scope.launch {
+            var i = 0
+            while(true) {
+                Log.v("MqttService", "Count: $i")
+                publish(i.toString().toByteArray())
+                i++
+                delay(1000)
+            }
+        }
         return START_STICKY
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        client.unregisterResources()
-        client.close()
-        client.disconnect()
-        serviceJob.cancel()
-        connectRetryCh.cancel()
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(randomNumberReceiver)
-    }
+    @ExperimentalCoroutinesApi
+    suspend fun connect() {
+        Log.d("MqttService", "connect")
 
-    private fun connect() {
-        if (!client.isConnected) {
-            Log.d("MqttService", "Try to connect")
+        val connectOptions = MqttConnectOptions().apply {
+            isAutomaticReconnect = true
+            connectionTimeout = 5
+        }
 
-            val connectOptions = MqttConnectOptions().apply {
-                isAutomaticReconnect = true
+        mutex.withLock {
+            if (client.isConnected) {
+                Log.d("MqttService", "connect")
+                return
             }
 
-            client.connect(connectOptions, null, object : IMqttActionListener {
-                override fun onSuccess(asyncActionToken: IMqttToken?) {
-                    Log.i("MqttService", "connected")
-                    LocalBroadcastManager
-                        .getInstance(this@MqttService)
-                        .registerReceiver(randomNumberReceiver, IntentFilter("randomNumber"))
-                    connectRetryCh.cancel()
-                    connectRetryJob.cancel()
-                }
+            withTimeout(10_000L) {
+                channelFlow {
+                    val listener = object : IMqttActionListener {
+                        override fun onSuccess(asyncActionToken: IMqttToken?) {
+                            Log.i("MqttService", "connected")
+                            launch { send(null) }
+                        }
 
-                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
-                    Log.e("MqttService", "failed to connect")
-                    serviceScope.launch {
-                        delay(1000)
-                        connectRetryCh.send(true)
+                        override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                            Log.e("MqttService", "connection error", exception)
+                            launch { send(null) }
+                        }
+                    }
+
+                    client.connect(connectOptions, this, listener)
+
+                    awaitClose()
+                }.first()
+            }
+        }
+    }
+
+    @ExperimentalCoroutinesApi
+    suspend fun disconnect() {
+        Log.d("MqttService", "disconnect")
+
+        mutex.withLock {
+            val e = channelFlow {
+                val listener = object : IMqttActionListener {
+                    override fun onSuccess(asyncActionToken: IMqttToken?) {
+                        Log.i("MqttService", "disconnected")
+                        launch { send(null) }
+                    }
+
+                    override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                        Log.i("MqttService", "disconnection error")
+                        launch { send(exception) }
                     }
                 }
-            })
+
+                client.disconnect(1000, this, listener)
+
+                awaitClose()
+            }.first()
+
+            if (e != null) {
+                val expectedErrorReasonCode = listOf(
+                    MqttException.REASON_CODE_CLIENT_ALREADY_DISCONNECTED.toInt(),
+                    MqttException.REASON_CODE_CLIENT_CLOSED.toInt(),
+                    MqttException.REASON_CODE_CLIENT_DISCONNECTING.toInt()
+                )
+
+                if (e is MqttException && expectedErrorReasonCode.contains(e.reasonCode)) {
+                    Log.i("MqttService", "expected errors on disconnection")
+                } else {
+                    Log.e("MqttService", "failed to disconnect")
+                    return
+                }
+            }
+
+            if (client is MqttAndroidClient) {
+                (client as MqttAndroidClient).unregisterResources()
+            } else {
+                client.close()
+                client = createClient()
+                Log.i("MqttService", "disconnection completed")
+            }
+
+            Unit
         }
+    }
+
+    private suspend fun publish(payload: ByteArray) {
+        mutex.withLock {
+            Log.i("MqttService", "publish")
+            if (!client.isConnected) {
+                Log.i("MqttService", "not published because client is not connected")
+                return
+            }
+            val msg = MqttMessage()
+            msg.payload = payload
+            client.publish("/test", msg)
+        }
+    }
+
+    private fun createClient(): IMqttAsyncClient {
+        val url = "tcp://10.0.2.2:1883"
+        val clientId = MqttClient.generateClientId()
+
+        val client = if (BuildConfig.MQTT_CLIENT == "ANDROID") {
+            MqttAndroidClient(this, url, clientId)
+        } else {
+            val dir = applicationContext.getExternalFilesDir("mqtt")
+            val persistence = MqttDefaultFilePersistence(dir!!.absolutePath)
+            MqttAsyncClient(url, clientId, persistence)
+        }
+
+        client.setCallback(object : MqttCallbackExtended {
+            override fun connectComplete(reconnect: Boolean, serverURI: String?) {
+                Log.v("MqttService", "connectComplete - ${client.clientId}")
+            }
+
+            override fun connectionLost(cause: Throwable?) {
+                Log.v("MqttService", "connectionLost - ${client.clientId}")
+            }
+
+            override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                Log.v("MqttService", "deliveryComplete - ${client.clientId}")
+            }
+
+            override fun messageArrived(topic: String?, message: MqttMessage?) {
+                Log.v("MqttService", "messageArrived - ${client.clientId}")
+            }
+        })
+
+        return client
     }
 }
