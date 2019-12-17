2ded71d226daf0ec1cdcf443cdff4d5db32d9bce
YUTA SHIMAKAWA <banana.umai@gmail.com>
2019-12-17 11:21:49 +0900

    implement own AlarmPingSender

--- mqtt/app/src/main/java/dev/bananaumai/android/suburi/mqtt/MqttService.kt
+++ mqtt/app/src/main/java/dev/bananaumai/android/suburi/mqtt/MqttService.kt
@@ -1,15 +1,24 @@
 package dev.bananaumai.android.suburi.mqtt
 
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import android.os.PowerManager
+import android.os.PowerManager.WakeLock
 import android.util.Log
 import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
+import org.eclipse.paho.client.mqttv3.internal.ClientComms
 import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence
 
 class MqttService : Service() {
@@ -118,7 +127,7 @@ class MqttService : Service() {
                 if (e.reasonCode == MqttException.REASON_CODE_CLIENT_TIMEOUT.toInt()) {
                     Log.d("MqttService", "connection process timed out")
                 } else {
-                    Log.e("MqttService", "failed to connect")
+                    Log.e("MqttService", "failed to connect", e)
                 }
             }
 
@@ -211,7 +220,8 @@ class MqttService : Service() {
             "NATIVE" -> {
                 val dir = applicationContext.getExternalFilesDir("mqtt")
                 val persistence = MqttDefaultFilePersistence(dir!!.absolutePath)
-                MqttAsyncClient(url, clientId, persistence)
+                val pingSender = AlarmPingSender(this)
+                MqttAsyncClient(url, clientId, persistence, pingSender)
             }
             else -> {
                 throw RuntimeException("invalid client")
@@ -237,5 +247,125 @@ class MqttService : Service() {
         })
 
         return client
+    }
+}
+
+class AlarmPingSender(private val service: Service) : MqttPingSender {
+    companion object {
+        // Identifier for Intents, log messages, etc..
+        private const val TAG = "AlarmPingSender"
+    }
+
+    private lateinit var comms: ClientComms
+    private lateinit var alarmReceiver: BroadcastReceiver
+
+    @Volatile
+    private var hasStarted = false
+
+    private var pendingIntent: PendingIntent? = null
+
+    override fun init(comms: ClientComms) {
+        this.comms = comms
+        alarmReceiver = AlarmReceiver()
+    }
+
+    override fun start() {
+        if (hasStarted) {
+            return
+        }
+
+        Log.d(TAG, "register alarm receiver to ${service.javaClass.simpleName}")
+
+        val action = "${service.javaClass.simpleName}_${comms.client.clientId}"
+
+        service.registerReceiver(alarmReceiver, IntentFilter(action))
+
+        pendingIntent = PendingIntent.getBroadcast(
+            service,
+            0,
+            Intent(action),
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        schedule(comms.keepAlive)
+
+        hasStarted = true
+    }
+
+    override fun stop() {
+        if (!hasStarted) {
+            return
+        }
+
+        Log.d(TAG, "unregister alarm receiver to ${service.javaClass.simpleName}")
+
+        if (pendingIntent != null) {
+            val alarmManager = service.getSystemService(Service.ALARM_SERVICE) as AlarmManager
+            alarmManager.cancel(pendingIntent)
+        }
+
+        hasStarted = false
+
+        try {
+            service.unregisterReceiver(alarmReceiver)
+        } catch (e: IllegalArgumentException) {
+            //Ignore unregister errors.
+        }
+    }
+
+    override fun schedule(delayInMilliseconds: Long) {
+        val alarmManager = service.getSystemService(Service.ALARM_SERVICE) as AlarmManager
+
+        val nextAlarmInMilliseconds = (System.currentTimeMillis() + delayInMilliseconds)
+
+        Log.d(TAG, "schedules alarm in next $delayInMilliseconds milliseconds, at $nextAlarmInMilliseconds")
+
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            nextAlarmInMilliseconds,
+            pendingIntent
+        )
+    }
+
+    internal inner class AlarmReceiver : BroadcastReceiver() {
+        private var wakelock: WakeLock? = null
+        private val wakeLockTag = "${service.javaClass.simpleName}-${javaClass.simpleName}-${comms.client.clientId}"
+
+        override fun onReceive(context: Context, intent: Intent) {
+            // According to the docs, "Alarm Manager holds a CPU wake lock as
+            // long as the alarm receiver's onReceive() method is executing.
+            // This guarantees that the phone will not sleep until you have
+            // finished handling the broadcast.", but this class still get
+            // a wake lock to wait for ping finished.
+            Log.d(TAG, "sending Ping at: ${System.currentTimeMillis()}")
+            val pm = service.getSystemService(Service.POWER_SERVICE) as PowerManager
+
+            wakelock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, wakeLockTag)
+
+            wakelock?.acquire(60_000L)
+
+            // Assign new callback to token to execute code after PingResq
+            // arrives. Get another wakelock even receiver already has one,
+            // release it until ping response returns.
+            val listener = object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken) {
+                    Log.d(TAG, "succeeded, release lock($wakeLockTag): ${System.currentTimeMillis()}")
+                    wakelock?.release()
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
+                    Log.d(TAG, "failed, release lock($wakeLockTag): ${System.currentTimeMillis()}")
+                    wakelock?.release()
+                }
+            }
+            val token: IMqttToken? = comms.checkForActivity(listener)
+
+            if (token == null) {
+                val isHeld = wakelock?.isHeld ?: false
+                if (isHeld) {
+                    wakelock?.release()
+                }
+            }
+        }
     }
 }
